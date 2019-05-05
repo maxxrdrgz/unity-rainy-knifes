bf7e5b7f655ee99b17a94e33b74eab63a4cbc11d
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-05-05 18:57:31 +0000

    commented  the playermove script

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,20 +5,31 @@ using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Text timer_Text;
+
     private Animator anim;
     private SpriteRenderer sr;
     private float speed = 3f;
     private float min_X = -2.7f;
     private float max_X = 2.7f;
-    public Text timer_Text;
     private int timer;
-    // Start is called before the first frame update
+
+    /**
+        Initializes components from the gameObject that will be manipulated
+
+        @returns {void}
+     */
     void Awake()
     {
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
     }
 
+    /**
+        starts the timers that is displayed on the main UI
+        
+        @returns {void}
+     */
     private void Start()
     {
         Time.timeScale = 1f;
@@ -26,13 +37,24 @@ public class PlayerMove : MonoBehaviour
         timer = 0;
     }
 
-    // Update is called once per frame
+    /**
+        checks the player bounds to ensure he stays within the screen view and 
+        updates player movement.
+        
+        @returns {void}
+     */
     void Update()
     {
         Move();
         PlayerBounds();
     }
 
+    /**
+        Gets the gameObjects transform position and checks if its between min 
+        and max X bounds
+
+        @returns {void}
+     */
     void PlayerBounds()
     {
         Vector3 temp = transform.position;
@@ -47,6 +69,12 @@ public class PlayerMove : MonoBehaviour
         transform.position = temp;
     }
 
+    /**
+        Gets horizontal input from user and changes the X axis of the 
+        gameObjects transform.position
+
+        @returns {void}
+     */
     void Move()
     {
         float h = Input.GetAxisRaw("Horizontal");
@@ -71,12 +99,23 @@ public class PlayerMove : MonoBehaviour
         transform.position = temp;
     }
 
+    /**
+        Coroutine for restarting the game. When called, this function will wait 
+        for a specific time frame and then reaload the current scene.
+
+        @returns {IEnumarator} returns a time delay before continuing execution
+     */
     IEnumerator RestartGame()
     {
         yield return new WaitForSecondsRealtime(2f);
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 
+    /**
+        Starts a count and acts as the timer by calling itself recursively
+
+        @returns {IEnumerator} returns a time delay before continuing execution
+     */
     IEnumerator CountTime()
     {
         yield return new WaitForSeconds(1f);
@@ -85,6 +124,12 @@ public class PlayerMove : MonoBehaviour
         StartCoroutine(CountTime());
     }
 
+    /**
+        Detects collision with gameObjects with the tag of Knife and restarts the
+        game. Game over.
+
+        @returns {void}
+     */
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Knife")
@@ -93,6 +138,4 @@ public class PlayerMove : MonoBehaviour
             StartCoroutine(RestartGame());
         }    
     }
-
-
 } // class
