c77155e1ec172f85e2d8e9aa336c3084a23d34fd
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-05-05 19:33:36 +0000

    added comments for the knife spawner

--- Assets/Scripts/KnifeSpawner.cs
+++ Assets/Scripts/KnifeSpawner.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class KnifeSpawner : MonoBehaviour
 {
     public GameObject knife;
+
     private float min_X = -2.7f;
     private float max_X = 2.7f;
-    // Start is called before the first frame update
+
+    /**
+        Initializes the knife spawner
+
+        @returns {void}
+     */
     void Start()
     {
         StartCoroutine(StartSpawning());
     }
 
+    /**
+        Coroutine that calls itself recursively for instantiating knife 
+        gameObjects between the min and max X bounds 
+
+        @returns {IEnumerator} returns a random time delay before continuing 
+        execution
+     */
     IEnumerator StartSpawning()
     {
         yield return new WaitForSeconds(Random.Range(0.5f, 1f));
